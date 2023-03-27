0095a24b51fcc254e5d4b4dc490adf0079e966b2
Titi <95020920+Tristitititi@users.noreply.github.com>
2023-03-27 10:20:28 +0200

    Install Breeze

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'microsoft' => [
+        'client_id' => env('MICROSOFT_CLIENT_ID'),
+        'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
+        'redirect' => env('MICROSOFT_REDIRECT_URI'),
+    ],
 ];
